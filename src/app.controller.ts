787812c631884fbb022a9795acf5ff6aff90d9d6
Felipe <fepipe82@hotmail.com>
2024-02-18 14:35:41 -0300

    Clase 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,52 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    const controller = UsersController;
+    console.log('path', Reflect.getMetadata('path', controller));
+    console.log('paths', Reflect.getMetadata('paths', controller));
     return this.appService.getHello();
+  }
+}
+
+//decorador de metodo
+function MetodoGet(path: string) {
+  return function (
+    target: any,
+    propertykey: string,
+    //descriptor: PropertyDescriptor,
+  ): any {
+    if (!Reflect.hasMetadata('paths', target.constructor)) {
+      Reflect.defineMetadata('paths', [], target.constructor);
+    }
+
+    const paths = Reflect.getMetadata('paths', target.constructor);
+
+    paths.push({
+      path,
+      verb: 'get',
+      methodName: propertykey,
+    });
+
+    Reflect.defineMetadata('paths', paths, target.constructor);
+  };
+}
+
+//decorador de clase
+function Controlador(path: string) {
+  return function (constructor: any) {
+    console.log('Controlador', path);
+    Reflect.defineMetadata('path', path, constructor);
+  };
+}
+
+@Controlador('/users')
+export class UsersController {
+  constructor() {
+    //console.log('UsersController');
+  }
+
+  @MetodoGet('/list')
+  List() {
+    console.log('list');
   }
 }
